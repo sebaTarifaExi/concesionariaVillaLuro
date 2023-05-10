3fc626d7b2a67d23fa441f7bd7bb43d32f69028e
Sebastian Tarifa <starifa@exisoft.com.ar>
2023-05-10 13:06:32 -0300

    Se creo el configuration

---
+++ src/main/java/com/concesionaria/villaLuro/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package com.concesionaria.villaLuro.controller;
+
+import com.concesionaria.villaLuro.service.ClienteService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alumno")
+public class ClienteController {
+
+    private ClienteService clienteService;
+
+
+
+}
